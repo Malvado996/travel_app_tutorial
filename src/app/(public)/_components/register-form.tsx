4f487b9152aa7b99e3dde618a72717f1fc8d5f6f
Philip Rodrigues <philip.rodrigues.js@gmail.com>
2026-01-20 21:56:22 -0500

    building form

--- src/app/(public)/_components/register-form.tsx
+++ src/app/(public)/_components/register-form.tsx
@@ -1,4 +1,34 @@
+'use client'
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+const formSchema = z.object({
+    title: z
+        .string()
+        .min(5, "Bug title must be at least 5 characters.")
+        .max(32, "Bug title must be at most 32 characters."),
+    description: z
+        .string()
+        .min(20, "Description must be at least 20 characters.")
+        .max(100, "Description must be at most 100 characters."),
+})
+
 function ResgisterForm() {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+            description: "",
+        },
+    })
+
+    function onSubmit(data: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        console.log(data)
+    }
+
     return (
         <div>
             Register Form
